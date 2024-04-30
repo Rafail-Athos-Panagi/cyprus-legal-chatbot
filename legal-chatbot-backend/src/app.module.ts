26e1adb29d7d1796ef2a84abeb8add1d5150f144
RaphaelPanayi <raphael.athos.panayi@gmail.com>
2024-04-30 15:46:59 +0300

    update

--- legal-chatbot-backend/src/app.module.ts
+++ legal-chatbot-backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthModule } from 'src/auth/auth.module';
-import { AtGuard } from 'src/common/guards';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { AtGuard } from './common/guards';
+import { PrismaModule } from './prisma/prisma.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { ModelModule } from 'src/model/model.module';
+import { ModelModule } from './model/model.module';
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, ModelModule ,PrismaModule , ThrottlerModule.forRoot([{
     ttl: 60000,
